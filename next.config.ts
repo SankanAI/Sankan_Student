86426750003ae8ce869f2843fe7ec2bb205cc83b
shashankyadahalli <shashankyadahalli@gmail.com>
2025-01-07 11:04:06 +0000

    Remove Unused

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ['raw.githubusercontent.com','github.com'], // Add the allowed hostname here
+  output: "export",  // Keep this for GitHub Pages
+  eslint: {
+    ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true,
+    domains: ['raw.githubusercontent.com', 'github.com'],
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Sankan_Student/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Sankan_Student' : '',
 };
 
 export default nextConfig;
